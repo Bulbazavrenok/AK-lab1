e3ddc56a22670f13951cecce9048c8b496610e19
Bulbazavrenok <98901552+Bulbazavrenok@users.noreply.github.com>
2022-09-20 18:04:35 +0300

    Add files via upload

---
+++ calculator/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+<<<<<<< HEAD
+        int Add (double, double);
+        int Sub (double, double);
+=======
+        static int Add (int, int);
+        static int Mul (double, double);
+>>>>>>> d61d32d... mark member-function as `static`
+};
+
+#endif//CALCULATOR_H
