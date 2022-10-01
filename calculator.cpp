244f59b1f98653fadadc65eaeadb70110131cbcc
Uliana <ulianasurg7@gmail.com>
2022-10-01 15:00:46 +0300

    UlianaKucherenko

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,13 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+//Cause I'm
+//T.N.T.
+//I'm dynamite
+//T.N.T.
+//And I'll win the fight
+//T.N.T.
+//I'm a power-load
+//T.N.T.
+//Watch me explode
